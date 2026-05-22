ecf973aa9881683dc84019a951bed0ac78164b16
Fahi Sabab, Al <feha.tasnim27@gmail.com>
2026-05-22 19:00:00 +0100

    made the hero section more visually appealing

--- app/components/home-page/sections/Hero.tsx
+++ app/components/home-page/sections/Hero.tsx
@@ -4,12 +4,59 @@ import { HOME_PAGE_SECTIONS } from "@/app/const";
 
 export default function Hero(){
     return (
-            <section className="min-h-screen flex items-center px-8 md:px-24" id="hero">
-                <div className="max-w-4xl">
-        
-                <p className="text-blue-400 text-sm tracking-widest uppercase mb-6">
-                    Portfolio 2026
-                </p>
+            <section id="hero"
+                className="
+                relative
+                min-h-screen
+                overflow-hidden
+                flex items-center
+                px-8 md:px-24
+                "
+            >
+               {/* Background image */}
+                <img
+                    src="/images/code.jpg"
+                    alt="code"
+                    className="
+                    absolute inset-0
+                    w-full h-full
+                    object-cover
+
+                    scale-110
+                    rotate-[-3deg]
+
+                    blur-md
+                    brightness-80
+                    opacity-60
+
+                    pointer-events-none
+                    select-none
+                    [mask-image:linear-gradient(to_bottom,black_70%,transparent_100%)]
+                    "
+                />
+
+                {/* Dark overlay */}
+                <div
+                    className="
+                    absolute inset-0
+                    bg-gradient-to-b
+                    from-black/50
+                    via-black/25
+                    to-black/80
+                    "
+                />
+
+                {/* Optional glow */}
+                <div
+                    className="
+                    absolute
+                    inset-0
+                    bg-blue-500/10
+                    blur-3xl
+                    "
+                />
+
+                <div className="max-w-4xl z-10">
         
                 <h1 className="text-6xl md:text-8xl font-semibold tracking-tight leading-tight">
                     Software Engineer
