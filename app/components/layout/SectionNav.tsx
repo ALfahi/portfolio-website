3d7f4fcd115f3faa284c99507040f5d01e55eea1
Fahi Sabab, Al <feha.tasnim27@gmail.com>
2026-05-23 01:19:27 +0100

    fixed sizing and positiining of the text inside the section nav

--- app/components/layout/SectionNav.tsx
+++ app/components/layout/SectionNav.tsx
@@ -5,10 +5,10 @@ import { scrollToSection } from "@/app/lib/scroll";
 
 export default function SectionNav() {
   return (
-    <div className="flex flex-wrap gap-6 justify-center mt-12 text-base md:text-lg">
+    <div className="flex flex-wrap gap-6 justify-start mt-12 text-label">
 
       <button onClick={() => scrollToSection("about")}
-        className="text-neutral-400 hover:text-white transition underline decoration-neutral-400 hover:decoration-white hover:cursor-pointer">
+        className="text-neutral-400 hover:text-white transition underline decoration-neutral-400 hover:decoration-white hover:cursor-pointer text-label">
         About
       </button>
 
