dc7e929a383cde4d0940f6c87aa21103b5057372
Fahi Sabab, Al <feha.tasnim27@gmail.com>
2026-05-24 13:23:08 +0100

    project item now also displays the github link per project as well as directing user to the specific github repo when the link is clicked

--- app/projects/components/projectList/ProjectItem.tsx
+++ app/projects/components/projectList/ProjectItem.tsx
@@ -4,6 +4,7 @@ import { Project } from "../../type";
 import Carousel from "../UI/carousel/Carousel";
 import TechPills from "./TechPills";
 import ProjectText from "./ProjectText";
+import { FaGithub } from "react-icons/fa";
 
 // displays a single project in the project list, with an image carousel on the right and description on the left (on desktop), 
 // and tech pills below (full span on desktop)
@@ -29,8 +30,47 @@ export default function ProjectItem({ project }: Props) {
       />
 
       {/* right: image */}
-      <div className="h-60 md:h-80 rounded-xl overflow-hidden">
-        <Carousel images={project.images} size="lg" />
+      <div className="space-y-4">
+        <div className="h-60 md:h-80 rounded-xl overflow-hidden">
+          <Carousel images={project.images} size="lg" />
+        </div>
+
+        {/* github link */}
+        <div className="flex items-center gap-3 text-neutral-400">
+          {project.githubLink ? (
+            <>
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="
+                  hover:text-blue-400 transition-colors
+                  hover:cursor-pointer
+                "
+              >
+                <FaGithub size={25} />
+              </a>
+
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="
+                  text-sm
+                  underline underline-offset-4 break-all
+                  hover:text-blue-400 transition-colors
+                "
+              >
+                {project.githubLink}
+              </a>
+            </>
+          ) : (
+            <>
+              <FaGithub size={25} className="text-neutral-500" />
+              <span className="text-body text-neutral-500">N/A</span>
+            </>
+          )}
+        </div>
       </div>
 
       {/* bottom (full span on desktop): tech pills */}
